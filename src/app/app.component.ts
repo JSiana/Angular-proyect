2aff8780b954f8da5a5be90db19f81efb67ccc38
JSiana <joaquinsiana2516@gmail.com>
2024-12-10 21:01:08 -0600

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: '',
 })
 export class AppComponent {
   title = 'angular-17-app';
